b6187241e0417924243522a500ff91048ff39c4b
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-21 00:38:51 -0400

    Watermelon

---
+++ Python/Watermelon.py
@@ -0,0 +1,8 @@
+	# 4A - Watermelon
+
+value = int(input())
+
+if value%2 is 0 and value > 3:
+    print("YES")
+else:
+    print("NO")
--- Python/Young Physicist.py
+++ Python/Young Physicist.py
@@ -3,7 +3,6 @@ n = int(input())
 x = 0
 y = 0
 z = 0
-
 for i in range(n):
     list_p = list(map(int,input().split(" ")))
     x += list_p[0]
@@ -13,6 +12,7 @@ if (x is 0  and y is 0 and z is 0):
     print("YES")
 else:
     print("NO")
+
 
 
 
