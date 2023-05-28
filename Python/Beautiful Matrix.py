ea85e7f57bc1c2078d12e44331895b5df6b2108b
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 21:53:25 -0400

    beautiful matrix v1

---
+++ Python/Beautiful Matrix.py
@@ -0,0 +1,22 @@
+# 	263A - Beautiful Matrix
+matrix = []
+col_index = 0;
+raw_index = 0;
+cost = 0
+for i in range (5):
+    lst = [int(x) for x in input().split()]
+    matrix.append(lst)
+for x in range(5):
+    for y in range(5):
+        if matrix[x][y] == 1:
+            col_index = y
+            raw_index = x
+if col_index > 2:
+    cost = (col_index-2)
+else:
+    cost = cost + (2-col_index)
+if raw_index > 2:
+    cost += (raw_index-2)
+else:
+    cost +=  (2-raw_index)
+print(cost)
