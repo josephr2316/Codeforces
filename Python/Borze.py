57b87bd691838f035fd6ca98071a19718d3f3832
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-06-09 22:43:10 -0400

    Borze

--- Python/Borze.py
+++ Python/Borze.py
@@ -1,28 +1,11 @@
-word = input()
-size = len(word)
-new_word = []
-result = ''
-if word[0] is '.':
-    result += '0'
-    for x in range(1,size,2):
-        if x == size-1:
-            new_word.append(word[x])
-        else:
-            new_word.append(word[x]+word[x+1])
-else:
-    for x in range(0,size,2):
-        if x == size-1:
-            new_word.append(word[x])
-        else:
-            new_word.append(word[x]+word[x+1])
-for val in  new_word:
-    if val == '.':
-        result += '0'
-    elif val == '-.':
-        result += '1'
-    elif val == '--':
-        result += '2'   
-print(result)
+#   32B - Borze
+if __name__ == '__main__':
+    word = input()
+    size = len(word)
+    word = word.replace("--",'2')
+    word = word.replace("-.",'1')
+    word = word.replace(".",'0')
+    print(word)
 
 
 
