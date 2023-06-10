9c88ee44a275807ad0c03557c16bfac64b053a0c
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-06-10 13:39:32 -0400

    Panoramix's Prediction

---
+++ Python/Panoramix's Prediction.py
@@ -0,0 +1,19 @@
+# 	80A - Panoramix's Prediction
+def prime_number(number):
+    for x in range(2,number):
+        if number % x == 0:
+            return False
+    return True
+
+
+
+if __name__ == '__main__':
+    lst = [int(x) for x in input().split(" ")]
+    prime_lst = []
+    for m in range(lst[0],lst[1] + 1):
+        if prime_number(m):
+            prime_lst.append(m)
+    if len(prime_lst)> 1 and prime_lst[1] == lst[1] :
+        print('YES')
+    else:
+        print('NO')
