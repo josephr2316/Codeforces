4c07dd63e827be6d9eb3b5b33f5b56e54260c377
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-06-10 13:00:47 -0400

    Beautiful year v2

--- Python/Beautiful Year.py
+++ Python/Beautiful Year.py
@@ -4,6 +4,10 @@ if __name__ == '__main__':
     while True:
         year +=1
         str_year = str(year)
-        if len(set(str_year)) == 4:
+        a = str_year.count(str_year[0])
+        b = str_year.count(str_year[1])
+        c = str_year.count(str_year[2])
+        d = str_year.count(str_year[3])
+        if a == 1 and b == 1 and c == 1 and d == 1:
             print(year)
             break
