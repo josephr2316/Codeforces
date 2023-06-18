0bd21616edaa6610880ee18f737c5f04d56c0702
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-06-18 16:07:06 -0400

    Helpful Maths v2

--- Python/Helpful Maths.py
+++ Python/Helpful Maths.py
@@ -1,6 +1,13 @@
 # 339A - Helpful Maths
 if __name__ == '__main__' :
-    number = input().split('+')
-    number.sort()
-    number = '+'.join(number)
-    print(number)
+    s = input()
+    n1 = n2 = n3 = 0
+    for i in range(0, len(s), 2):
+        if s[i] == '1':
+            n1 += 1
+        elif s[i] == '2':
+            n2 += 1
+        else:
+            n3 += 1
+    ss = "1+" * n1 + "2+" * n2 + "3+" * n3
+    print(ss[:-1])
