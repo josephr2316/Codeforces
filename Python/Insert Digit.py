4057beb1b113254a1d6a15a364f7c3b44d71a80c
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-06-24 02:53:20 -0400

    Insert Digit v3

--- Python/Insert Digit.py
+++ Python/Insert Digit.py
@@ -4,18 +4,23 @@ if __name__ == '__main__' :
     for x in range(cases):
         lst = list(map(int,input().split()))
         word = input()
-        for m in range(lst[0]):
-            if int(word[m]) < lst[1] and m == 0:
-                new_lst = list(word)
-                new_lst.insert(m,lst[1])
-                break;
-            elif m < lst[0] -1 and int(word[m]) >= lst[1] and int(word[m + 1]) < lst[1] :
-                new_lst = list(word)
-                new_lst.insert(m + 1,lst[1])
-                break;
-            elif m == lst[0] -1 :
-                new_lst = list(word)
-                new_lst.append(str(lst[1]))
-                break;
-        print(''.join([str(x) for x in new_lst]))
+        is_bigger = False
+        x = 0
+        for m in word:
+            if int(m) < lst[1]  and not is_bigger:
+                print(str(lst[1]),end="")
+                print(m,end="")
+                is_bigger = True
+            else :
+                print(m,end="")
+                x += 1
+        if not(is_bigger) :
+            print(str(lst[1]))
+        else:
+            print()
 
+                
+            
+
+
+            
