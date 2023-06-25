e35d8bcbd446b11901b2e1764a2233d99474245b
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-06-25 00:00:02 -0400

    Effective Approach

--- Python/Effective Approach.py
+++ Python/Effective Approach.py
@@ -2,12 +2,15 @@
 if __name__ == '__main__' :
     size = int(input())
     lst_array = [int(x) for x in input().split()]
+    lst = [0 for i in range(size+1)]
     query_size = int(input())
     lst_querries = [int(x) for x in input().split()]
     Vasya = 0
     Petya = 0
+    for y in range(size) :
+        lst[lst_array[y]] = y
     for query in lst_querries :
-        index = lst_array.index(query)
+        index = lst[query]
         Vasya +=  index + 1
         Petya += size - index
     print(f'{Vasya} {Petya}')
