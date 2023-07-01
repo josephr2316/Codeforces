c86cddf5436f75f93fb85fe7befbc2c589fa543e
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-07-01 16:49:14 -0400

    Juzzhu and Children

--- Python/Jzzhu and Children.py
+++ Python/Jzzhu and Children.py
@@ -1,13 +1,25 @@
 # 	450A - Jzzhu and Children
 if __name__== '__main__' :
-    n,m = input().split()
+    n,m = [int(x) for x in input().split()]
     lst = [int(x) for x in input().split()]
-    max = 0
     position = 0
     x = 0
-    for element in lst:
-        x += 1
-        if element >= max :
-            max = element
-            position = x
-    print(position)
+    y = 0
+    is_max = False
+    while x < n :
+        if lst[y] != 0 :
+            if lst[y] - m <= 0 :
+                lst[y] = 0
+                x += 1
+            else :
+                lst[y] = lst[y] - m
+                is_max = True
+                position = y
+        y += 1 
+        if y == n :
+            y = 0
+    if not(is_max):
+        print(n)
+    else :
+        print(position+1)
+   
