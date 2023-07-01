c84c0f93e4adbd763de812ceaf0af0a72a24f4bd
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-07-01 16:59:57 -0400

    Jzzhu and Children v2

--- Python/Jzzhu and Children.py
+++ Python/Jzzhu and Children.py
@@ -1,25 +1,15 @@
 # 	450A - Jzzhu and Children
+import math
 if __name__== '__main__' :
     n,m = [int(x) for x in input().split()]
     lst = [int(x) for x in input().split()]
     position = 0
-    x = 0
-    y = 0
-    is_max = False
-    while x < n :
-        if lst[y] != 0 :
-            if lst[y] - m <= 0 :
-                lst[y] = 0
-                x += 1
-            else :
-                lst[y] = lst[y] - m
-                is_max = True
-                position = y
-        y += 1 
-        if y == n :
-            y = 0
-    if not(is_max):
-        print(n)
-    else :
-        print(position+1)
-   
+    max = 0
+    i = 1
+    for number in lst :
+        if math.ceil(number/m) >= max :
+            max = math.ceil(number/m)
+            position = i
+        i += 1
+    print(position)
+
