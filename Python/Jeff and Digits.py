3c411118929194989b4ae6294790eb330c95d3ea
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-07-07 21:22:15 -0400

    Jeff and Digits

--- Python/Jeff and Digits.py
+++ Python/Jeff and Digits.py
@@ -1,16 +1,12 @@
-#
+# 352A - Jeff and Digits
 if __name__ == '__main__' :
-    size = int(input())
-    numbers = input()
-    w = ''
-    is_true = False
-    for number in numbers.split() :
-        w+=number
-        print(w)
-        if int(w) % 90 == 0 :
-            print(w)
-            is_true = True
-            break
+    size,numbers = int(input()), input()
+    zero_numbers = numbers.count('0')
+    five_numbers = numbers.count('5')
+    five_numbers -= five_numbers % 9
+    if zero_numbers == 0 : print(-1)
+    elif five_numbers < 9 : print(0)
+    else :
+        print(f"{'5'*five_numbers}{'0'*zero_numbers}")
 
-    if not(is_true):
-        print(0)
+    
